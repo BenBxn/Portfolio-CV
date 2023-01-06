41722a7cfd210c4fb58261c52bfcd3ac92cd4f78
BenBxn <dessortbxn@gmail.com>
2023-01-06 23:50:42 +0100

    Maj portfolio

--- js/global.js
+++ js/global.js
@@ -101,5 +101,119 @@ function showProjectDetails() {
 showProjectDetails();
 
 
+/* ***************** text animation  ************* */
+const resolver = {
+  resolve: function resolve(options, callback) {
+    // The string to resolve
+    const resolveString = options.resolveString || options.element.getAttribute('data-target-resolver');
+    const combinedOptions = Object.assign({}, options, {resolveString: resolveString});
+    
+    function getRandomInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+    
+    function randomCharacter(characters) {
+      return characters[getRandomInteger(0, characters.length - 1)];
+    };
+    
+    function doRandomiserEffect(options, callback) {
+      const characters = options.characters;
+      const timeout = options.timeout;
+      const element = options.element;
+      const partialString = options.partialString;
+
+      let iterations = options.iterations;
+
+      setTimeout(() => {
+        if (iterations >= 0) {
+          const nextOptions = Object.assign({}, options, {iterations: iterations - 1});
+
+          // Ensures partialString without the random character as the final state.
+          if (iterations === 0) {
+            element.textContent = partialString;
+          } else {
+            // Replaces the last character of partialString with a random character
+            element.textContent = partialString.substring(0, partialString.length - 1) + randomCharacter(characters);
+          }
+
+          doRandomiserEffect(nextOptions, callback)
+        } else if (typeof callback === "function") {
+          callback(); 
+        }
+      }, options.timeout);
+    };
+    
+    function doResolverEffect(options, callback) {
+      const resolveString = options.resolveString;
+      const characters = options.characters;
+      const offset = options.offset;
+      const partialString = resolveString.substring(0, offset);
+      const combinedOptions = Object.assign({}, options, {partialString: partialString});
+
+      doRandomiserEffect(combinedOptions, () => {
+        const nextOptions = Object.assign({}, options, {offset: offset + 1});
+
+        if (offset <= resolveString.length) {
+          doResolverEffect(nextOptions, callback);
+        } else if (typeof callback === "function") {
+          callback();
+        }
+      });
+    };
+
+    doResolverEffect(combinedOptions, callback);
+  } 
+}
+
+/* text
+ */
+const strings = [
+  'Qui suis-je ?',
+  'Hier, Animateur, référent jeunesse, directeur d\'un espace jeunesse et de séjours en Europe.',
+  'Aujourd\'hui, j\'ai décidé de donner un nouveau sens à ma carrière.',
+  'Dans un monde en perpétuelle évolution, ma curiosité est insatiable.',
+  'J\’ai amélioré et transmis cette compétence auprès d\’un large public,',
+  'en mettant en place divers projets de découverte, de prévention et de création sur les outils numériques.',
+  'Elle m\'a amené à découvrir le monde du développement et du web : « Un monde avec des ressources en abondance ».',
+  'Mon empathie et mes valeurs pédagogiques ont fortifié mon orientation dans ce nouveau domaine.',
+  'Je peux ainsi continuer d\’aider les personnes à avoir accès aux informations facilement et contribuer à la sécurité et à l\’entraide,',
+  'avec un sens du collectif et une éthique professionnelle modèles pour une entreprise.',
+  '......'
+];
+
+let counter = 0;
+
+const options = {
+  // Initial position
+  offset: 0,
+  // Timeout between each random character
+  timeout: 5,
+  // Number of random characters to show
+  iterations: 10,
+  // Random characters to pick from
+  characters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'x',],
+  // String to resolve
+  resolveString: strings[counter],
+  // The element
+  element: document.querySelector('[data-target-resolver]')
+}
+
+// Callback function when resolve completes
+function callback() {
+  setTimeout(() => {
+    counter ++;
+    
+    if (counter >= strings.length) {
+      counter = 0;
+    }
+    
+    let nextOptions = Object.assign({}, options, {resolveString: strings[counter]});
+    resolver.resolve(nextOptions, callback);
+  }, 1000);
+}
+
+resolver.resolve(options, callback);
 
 
+
+/* ***************** CAROUSEL ************* */
